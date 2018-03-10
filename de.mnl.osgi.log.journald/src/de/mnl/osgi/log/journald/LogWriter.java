6e6b714f3086d90dded1e1a4605bf369423241a8
Michael N. Lipp <mnl@mnl.de>
2018-03-10 10:50:28 +0100

    To be continued.

---
+++ de.mnl.osgi.log.journald/src/de/mnl/osgi/log/journald/LogWriter.java
@@ -0,0 +1,14 @@
+package de.mnl.osgi.log.journald;
+
+import org.osgi.service.log.LogEntry;
+import org.osgi.service.log.LogListener;
+
+public class LogWriter implements LogListener {
+
+	@Override
+	public void logged(LogEntry entry) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
