9b88dae80b18e80a59ee8f42b185d9b9e409b93c
Michael N. Lipp <mnl@mnl.de>
2019-02-07 13:27:23 +0100

    Fixed copyright notice.

--- de.mnl.osgi.osgi2jul/src/de/mnl/osgi/osgi2jul/LogWriter.java
+++ de.mnl.osgi.osgi2jul/src/de/mnl/osgi/osgi2jul/LogWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Michael N. Lipp (http://www.mnl.de)
+ * Copyright (C) 2019 Michael N. Lipp (http://www.mnl.de)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
