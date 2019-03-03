abda4bc5100fbedd997253cc51e607afe2a8e467
Michael N. Lipp <mnl@mnl.de>
2019-03-03 14:26:40 +0100

    Usage comment.

--- de.mnl.osgi.slf4j2osgi/src/org/slf4j/impl/package-info.java
+++ de.mnl.osgi.slf4j2osgi/src/org/slf4j/impl/package-info.java
@@ -22,8 +22,21 @@
  * <P>
  * Simply install this bundle in addition to de.mnl.osgi.lf4j and you
  * can use the SLF4J {@code LoggerFactory} as you're used to. As this
- * implementation shares everything "behind the facade" with LFOSGi,
+ * implementation shares everything "behind the facade" with LF4OSGi,
  * it also shares its configuration options.
+ * <P>
+ * If you want to use automatic resolution of bundles, make sure to
+ * exclude any other providers of the {@code org.slf4j.impl}. E.g.:
+ * <pre>
+ * # - SLF4J 1.8 is still alpha and backward compatibility is unclear
+ * # - Make sure that the import "org.slf4j.impl;version=1.6.0" is only 
+ * #   satisfied by de.mnl.osgi.slf4j2osgi.
+ * -runblacklist: \
+ *     osgi.identity;filter:='(&amp;(osgi.identity=slf4j.api)(version&gt;=1.8.0))',\
+ *     osgi.identity;filter:='(osgi.identity=slf4j.simple)',\
+ *     osgi.identity;filter:='(osgi.identity=slf4j.log4j12)',\
+ *     ... (more implementation bundles in your repositories)
+ * </pre>
  */
 @Version("1.6.0") // API requires an impl with at least this version
 package org.slf4j.impl;
