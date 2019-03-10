63b226fc5aa6d15fbe369c0c899d51ce7e96750b
Michael N. Lipp <mnl@mnl.de>
2019-03-10 14:49:58 +0100

    Fixed indentation.

--- buildSrc/src/ConfigurePublishing.groovy
+++ buildSrc/src/ConfigurePublishing.groovy
@@ -6,7 +6,7 @@ import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 
 class ConfigurePublishing implements Plugin<Project> {
-	
+
 	void apply(Project project) {
 
 		project.extensions.create("configurePublishing", ConfigurePublishingExtension)
@@ -16,9 +16,9 @@ class ConfigurePublishing implements Plugin<Project> {
 				maven(MavenPublication) {
 					// Make available in closure below
 					def publication = it
-                    from project.components.java
-					
-                    project.afterEvaluate {
+					from project.components.java
+
+					project.afterEvaluate {
 						// Depend on jar built by bnd
 						def jarFile = project.components.java.artifacts.first().file
 						def pomTask = project.tasks.generatePomFileForMavenPublication
@@ -36,65 +36,65 @@ class ConfigurePublishing implements Plugin<Project> {
 							}
 
 						}
-					
-                        artifactId = project.archivesBaseName
-                        artifact(project.tasks.sourcesJar) {
-                            classifier = 'sources'
-                        }
-                        artifact(project.tasks.javadocJar) {
-                            classifier = 'javadoc'
-                        }
-                        pom.packaging = "jar"
-					
-                        // Until https://github.com/gradle/gradle/issues/1232 is fixed:
-                        pom.withXml {
-                            // Generate map of resolved versions
-                            Map resolvedVersionMap = [:]
-                            Set<ResolvedArtifact> resolvedArtifacts = project.configurations.compile.getResolvedConfiguration().getResolvedArtifacts()
-                            resolvedArtifacts.each {
-                                ModuleVersionIdentifier mvi = it.getModuleVersion().getId();
-                                resolvedVersionMap.put("${mvi.getGroup()}:${mvi.getName()}", mvi.getVersion())
-                            }
-                            Set<ResolvedArtifact> testResolved = project.configurations.testCompile.getResolvedConfiguration().getResolvedArtifacts()
-                            testResolved.each {
-                                ModuleVersionIdentifier mvi = it.getModuleVersion().getId();
-                                resolvedVersionMap.put("${mvi.getGroup()}:${mvi.getName()}", mvi.getVersion())
-                            }
 
-                            // Update dependencies with resolved versions
-                            if (asNode().dependencies) {
-                                asNode().dependencies.first().each {
-                                    def groupId = it.get("groupId").first().value().first()
-                                    def artifactId = it.get("artifactId").first().value().first()
-                                    def version = it.get("version").first().value()[0];
-                                    // Leave Maven version ranges alone.
-                                    if (!version.startsWith('(') && !version.startsWith('[')) {
-                                        it.get("version").first().value = resolvedVersionMap.get("${groupId}:${artifactId}")
-                                    }
-                                }
-                            }
-                        }
+						artifactId = project.archivesBaseName
+						artifact(project.tasks.sourcesJar) {
+							classifier = 'sources'
+						}
+						artifact(project.tasks.javadocJar) {
+							classifier = 'javadoc'
+						}
+						pom.packaging = "jar"
 
-                        def projectName = project.name
-                        def projectDescription = project.description
-                        if (projectDescription == null || projectDescription == "") {
-                            projectDescription = "(No description)"
-                        }
-                        pom.withXml {
-                            asNode().with {
-                                appendNode('name', projectName)
-                                appendNode('description', projectDescription)
-                            }
-                        }
-                        pom.withXml(project.configurePublishing.withPomXml)
-                    }
-                }
-            }
+						// Until https://github.com/gradle/gradle/issues/1232 is fixed:
+						pom.withXml {
+							// Generate map of resolved versions
+							Map resolvedVersionMap = [:]
+							Set<ResolvedArtifact> resolvedArtifacts = project.configurations.compile.getResolvedConfiguration().getResolvedArtifacts()
+							resolvedArtifacts.each {
+								ModuleVersionIdentifier mvi = it.getModuleVersion().getId();
+								resolvedVersionMap.put("${mvi.getGroup()}:${mvi.getName()}", mvi.getVersion())
+							}
+							Set<ResolvedArtifact> testResolved = project.configurations.testCompile.getResolvedConfiguration().getResolvedArtifacts()
+							testResolved.each {
+								ModuleVersionIdentifier mvi = it.getModuleVersion().getId();
+								resolvedVersionMap.put("${mvi.getGroup()}:${mvi.getName()}", mvi.getVersion())
+							}
+
+							// Update dependencies with resolved versions
+							if (asNode().dependencies) {
+								asNode().dependencies.first().each {
+									def groupId = it.get("groupId").first().value().first()
+									def artifactId = it.get("artifactId").first().value().first()
+									def version = it.get("version").first().value()[0];
+									// Leave Maven version ranges alone.
+									if (!version.startsWith('(') && !version.startsWith('[')) {
+										it.get("version").first().value = resolvedVersionMap.get("${groupId}:${artifactId}")
+									}
+								}
+							}
+						}
+
+						def projectName = project.name
+						def projectDescription = project.description
+						if (projectDescription == null || projectDescription == "") {
+							projectDescription = "(No description)"
+						}
+						pom.withXml {
+							asNode().with {
+								appendNode('name', projectName)
+								appendNode('description', projectDescription)
+							}
+						}
+						pom.withXml(project.configurePublishing.withPomXml)
+					}
+				}
+			}
 		}
-        
-        if (project.hasProperty("signing.keyId")) {
-            project.signing.sign(project.publishing.publications.maven)
-        }
+
+		if (project.hasProperty("signing.keyId")) {
+			project.signing.sign(project.publishing.publications.maven)
+		}
 
 	}
 
@@ -109,5 +109,5 @@ class ConfigurePublishing implements Plugin<Project> {
 			target.dependencies*.append(it)
 		}
 	}
-	
+
 }
