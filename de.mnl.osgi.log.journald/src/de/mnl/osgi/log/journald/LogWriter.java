82fa3ecf5b54ef1b46291fe37b35801bf5b32a54
Michael N. Lipp <mnl@mnl.de>
2021-11-28 22:55:36 +0100

    Fix checkstyle warnings.

--- de.mnl.osgi.log.journald/src/de/mnl/osgi/log/journald/LogWriter.java
+++ de.mnl.osgi.log.journald/src/de/mnl/osgi/log/journald/LogWriter.java
@@ -1,14 +1,38 @@
+/*
+ * Copyright (C) 2016 Michael N. Lipp (http://www.mnl.de)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.mnl.osgi.log.journald;
 
 import org.osgi.service.log.LogEntry;
 import org.osgi.service.log.LogListener;
 
+/**
+ * The Class LogWriter.
+ */
 public class LogWriter implements LogListener {
 
-	@Override
-	public void logged(LogEntry entry) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Logged.
+     *
+     * @param entry the entry
+     */
+    @Override
+    public void logged(LogEntry entry) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
